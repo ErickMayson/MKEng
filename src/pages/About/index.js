25bfb31013f03d5d30eaaf775329f6eba7e82eb3
ErickMayson <Ferreiraerick0101@gmail.com>
2023-07-13 18:50:11 -0300

    Added image and placeholder text

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,12 +1,23 @@
-import { ContentContainer, PageContainer } from './styles';
+import {
+  ContentContainer, PageContainer, Title, TitleContainer,
+} from './styles';
+import blueprint from '../../assets/images/blueprint.png';
 
 export default function About() {
   return (
     <div>
       <PageContainer>
+        <TitleContainer>
+          <Title>YABADABA DOOOOOOOOOOOO </Title>
+        </TitleContainer>
         <ContentContainer>
-          <div>ABALBABA</div>
-          <div>DSADSADSA</div>
+          <div className="contenttext">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Molestiae commodi qui dignissimos doloremque iure debitis,
+            eos aspernatur explicabo quisquam. Deleniti,
+            quaerat similique dolores quae minima error repellat ullam pariatur aspernatur?
+          </div>
+          <img src={blueprint} alt="MyContacts" />
         </ContentContainer>
       </PageContainer>
     </div>
--- src/pages/About/styles.js
+++ src/pages/About/styles.js
@@ -11,6 +11,37 @@ export const ContentContainer = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-evenly;
-  margin-top: 20px
+  margin-top: 20px;
 
+  .contenttext{
+    text-align: center;
+    color: #000000;
+    font-size: 16px;
+    width: 500px;
+  }
+
+  img {
+    border-radius: 5px;
+    width: 500px;
+  }
+
+`;
+
+export const Title = styled.div`
+  margin-top: 32px;
+  text-align: center;
+  justify-content:center;
+  font-size: 32px;
+  font-weight: bold;
+  border-bottom: 3px solid ${({ theme }) => theme.colors.primary.main};
+  display: flex;
+  width: 600px;
+  padding: 12px;
+  color:  ${({ theme }) => theme.colors.primary.main}
+`;
+
+export const TitleContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
 `;
